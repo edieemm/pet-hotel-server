9c761dcdb3d1836da7c4f381f452dd265d084f4b
Lilith Bentley <sbentley@gmail.com>
2019-10-31 10:11:01 -0500

    Add get_owners query

--- owners.py
+++ owners.py
@@ -3,15 +3,28 @@ import psycopg2
 import json
 
 
-# def get_creatures():
+def get_owners():
+    conn = None
+    try:
+        conn = psycopg2.connect("dbname=pet_hotel")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM owner;")
+        rows = cur.fetchall()
+        return(rows)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+# def add_owner():
 #     conn = None
 #     try:
-#         conn = psycopg2.connect("dbname=mythical_creatures")
-#         cur = conn.cursor()
-#         cur.execute("SELECT * FROM creatures;")
-#         rows = cur.fetchall()
-#         return(rows)
-#         cur.close()
+#         conn = psycopg2.connect("dbname=pet_hotel")
+#         cur = con.cursor()
+#         cur.execute("ADD")
 #     except (Exception, psycopg2.DatabaseError) as error:
 #         print(error)
 #     finally:
@@ -19,18 +32,10 @@ import json
 #             conn.close()
 
 
-# def add_creatures():
-#     conn = None
-#     try:
-#         conn = psycopg2.connect("dbname=mythical_creatures")
-#         cur = con.cursor()
-#         cur.execute("ADD")
-
-
 # app = Flask(__name__)
 
 
-# @app.route("/", methods=['GET', 'POST'])
-# def creatures():
-#     creatures = json.dumps(get_creatures())
-#     return(creatures)
+@app.route("/owner", methods=['GET', 'POST', 'DELETE'])
+def owners():
+    owners = json.dumps(get_owners())
+    return(owners)
