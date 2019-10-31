2055f0cfb6e09fe60b2df5d44cb230984fb2ea46
Mike Lynch <mjlynch2@gmail.com>
2019-10-31 10:31:18 -0500

    create get route that returns all pets in db

--- pets.py
+++ pets.py
@@ -1,24 +1,26 @@
 from flask import Flask, request, jsonify
+import psycopg2
 
 app = Flask(__name__)
 
 
 @app.route('/pets', methods=['GET', 'POST'])
-def pets_router():
+def pets_router1():
     if request.method == 'POST':
-
+        return 'POST'
     elif request.method == 'GET':
         pets = get_pets()
-
+        return jsonify(pets)
     else: 
         return 'No valid method requested'
 
 @app.route('/pets/<id_>', methods=['PUT', 'DELETE'])
 def pets_router():
     if request.method == 'PUT':
+        return 'PUT'
         # pets = get_pets()
     elif request.method == 'DELETE':
-
+        return 'DELETE'
     else: 
         return 'No valid method requested'
 
